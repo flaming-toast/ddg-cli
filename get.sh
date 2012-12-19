cccc3a4b4d0341816b4a6eb4831f378181b2cde5
Jessica Yu <kumimoko.yo@gmail.com>
2012-12-19 01:13:44 -0800

    removed cruft

--- get.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# grab json response from api.ddg, run it through parsing script parse.pl
-rootdir=$(pwd)
-query=$(echo $@ | sed 's/ /+/g')
-echo "Your query: $@"
-curl --silent "http://api.duckduckgo.com/?q=$query&format=json&pretty=1" | ${rootdir}/parse.pl -
